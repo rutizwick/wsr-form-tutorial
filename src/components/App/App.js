6bf1c4c64010e6bcb21f3f56a226e263e42b36fd
rutiz <rutiz@wix.com>
2021-04-12 15:09:51 +0300

    first commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types';
 import { withTranslation } from '@wix/wix-i18n-config';
 import s from './App.scss';
 
+import {
+  Page,
+  Container,
+  Row,
+  Col,
+  Card,
+  Box,
+  Input,
+  InputArea,
+  Dropdown,
+  Checkbox,
+  Button,
+  FormField,
+  Text,
+} from 'wix-style-react';
+
 class App extends React.Component {
   static propTypes = {
     t: PropTypes.func,
@@ -12,11 +28,9 @@ class App extends React.Component {
     const { t } = this.props;
 
     return (
-      <div className={s.root}>
-        <h2 className={s.title} data-hook="app-title">
-          {t('app.title', { who: 'Yoshi' })}
-        </h2>
-      </div>
+      <Page>
+        <Page.Header title="WSR Form" />
+      </Page>
     );
   }
 }
